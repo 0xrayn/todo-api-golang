f1c59eb14cc2768529fa811d7564be9fb0007960
0xrayn <rayndw02@gmail.com>
2026-06-07 20:04:39 +0700

    add user_id

--- handlers/todo.go
+++ handlers/todo.go
@@ -9,20 +9,23 @@ import (
 )
 
 func GetTodos(c *gin.Context) {
+	// Ambil userID dari token
+	userID := c.MustGet("userID")
+
 	var todos []models.Todo
-	database.DB.Find(&todos)
+	database.DB.Where("user_id = ?", userID).Find(&todos)
 
 	c.JSON(http.StatusOK, todos)
 }
 
 func GetTodo(c *gin.Context) {
+	userID := c.MustGet("userID")
 	id := c.Param("id")
 
 	var todo models.Todo
-
-	if err := database.DB.First(&todo, id).Error; err != nil {
+	if err := database.DB.Where("id = ? AND user_id = ?", id, userID).First(&todo).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
-			"message": "Todo not Found",
+			"message": "Todo tidak ditemukan",
 		})
 		return
 	}
@@ -31,34 +34,40 @@ func GetTodo(c *gin.Context) {
 }
 
 func CreateTodo(c *gin.Context) {
-	var todo models.Todo
+	// Ambil userID dari token
+	userID := c.MustGet("userID")
 
-	if err := c.ShouldBindJSON(&todo); err != nil {
+	var input models.Todo
+	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
 		})
 		return
 	}
 
-	database.DB.Create(&todo)
+	todo := models.Todo{
+		UserID:    uint(userID.(float64)),
+		Title:     input.Title,
+		Completed: input.Completed,
+	}
 
+	database.DB.Create(&todo)
 	c.JSON(http.StatusCreated, todo)
 }
 
 func UpdateTodo(c *gin.Context) {
+	userID := c.MustGet("userID")
 	id := c.Param("id")
 
 	var todo models.Todo
-
-	if err := database.DB.First(&todo, id).Error; err != nil {
+	if err := database.DB.Where("id = ? AND user_id = ?", id, userID).First(&todo).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
-			"message": "Todo not found",
+			"message": "Todo tidak ditemukan",
 		})
 		return
 	}
 
 	var input models.Todo
-
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
@@ -68,27 +77,25 @@ func UpdateTodo(c *gin.Context) {
 
 	todo.Title = input.Title
 	todo.Completed = input.Completed
-
 	database.DB.Save(&todo)
 
 	c.JSON(http.StatusOK, todo)
 }
 
 func DeleteTodo(c *gin.Context) {
+	userID := c.MustGet("userID")
 	id := c.Param("id")
 
 	var todo models.Todo
-
-	if err := database.DB.First(&todo, id).Error; err != nil {
+	if err := database.DB.Where("id = ? AND user_id = ?", id, userID).First(&todo).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
-			"message": "Todo not found",
+			"message": "Todo tidak ditemukan",
 		})
 		return
 	}
 
 	database.DB.Delete(&todo)
-
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Todo deleted",
+		"message": "Todo berhasil dihapus",
 	})
 }
--- models/todo.go
+++ models/todo.go
@@ -2,6 +2,7 @@ package models
 
 type Todo struct {
 	ID        uint   `json:"id" gorm:"primaryKey"`
+	UserID    uint   `json:"user_id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
